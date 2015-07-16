d0eda35a100ea98cc020a638441376a87d0d3ea3
Noah Scholnick <noahscholnick@gmail.com>
2015-07-16 11:48:32 -0400

    Initial Commit

---
+++ BizzyBee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BizzyBee
+//
+//  Created by Noah Scholnick on 7/16/15.
+//  Copyright (c) 2015 Noah Scholnick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
